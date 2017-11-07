231d504d92ae1464db5a982a7b986d9f1b12c035
Victor Presumido <victor.presumido@gmail.com>
2017-11-07 21:29:19 -0200

    Implement constructor for Player
    Player constructor and Player#play method

--- lib/rspotify/player.rb
+++ lib/rspotify/player.rb
@@ -1,10 +1,33 @@
 module RSpotify
   class Player < Base
 
-    def initialize(options = {})
+    def initialize(user, options = {})
+      @user = user
+
+      @repeat_state  = options['repeat_state']
+      @shuffle_state = options['shuffle_state']
+      @progress      = options['progress_ms']
+      @is_playing    = options['is_playing']
+
+      @track = if options['track']
+        Track.new options['track']
+      end
+
       @device = if options['device']
         Device.new options['device']
       end
+    end
+
+    def playing?
+      is_playing
+    end
+
+    def play(device_id = nil, options = {})
+      url = "me/player/play"
+      url = device_id.nil? ? url : url+"?device_id=#{device_id}"
+
+      request_body = options
+      User.oauth_put(@user.id, url, request_body)
     end
   end
 end
